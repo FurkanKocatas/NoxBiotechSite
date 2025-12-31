dd0ee22b6ccfce39112b1eb700adca0dbe7a9e4c
Meru <atheron112@gmail.com>
2025-12-31 16:15:36 +0300

    BirlestirilmisHal

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
@@ -6,7 +7,11 @@ export default defineConfig({
     assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
-      input: './index.html'
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        pipeline: resolve(__dirname, 'pipeline.html'),
+        purpose: resolve(__dirname, 'purpose.html')
+      }
     }
   },
   publicDir: 'assets'
